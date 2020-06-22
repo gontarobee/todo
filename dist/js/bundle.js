c089c7a2969f01ce381c4d1a565c0f5b35672f4e
高野義道 <>
2020-06-22 23:33:15 +0900

    2020_6_22(月)レッスン終了直後のupload

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -300,10 +300,8 @@ new _vue2.default({
     kensaku_value: '',
     max_id: -1, // add_todoして配列を最初に作るする際のidを「０」にしたいので、初期設定を−１としておく。こうすることでidとtodo_listの配列の番号を一致させる。
     min_id: '',
-    resulu_id: '',
-    del_idBox: [{
-      id: ''
-    }],
+    result_id: '',
+    del_idBox: [],
     todo_list: [],
     kensaku_todo_list: [],
     log: '追加ボタンが押され、「',
@@ -313,19 +311,32 @@ new _vue2.default({
   methods: {
     add_todo: function add_todo() {
 
-      if (isNaN(this.del_idBox[0].id) === false) {
-        //isNaN は数字でないとき「true」になる。よって数字の時は「false」となる。
-        this.min_id = this.del_idBox[0].id; // 最後に削除されたidを、min_idに代入。
-        this.result_id = this.min_id; // min_id　を　result_idに代入して、今回のid番号を確定させる。
-        this.del_idBox.shift(); //配列の先頭の要素を削除　→　削除後は１番目以降の数字が一つづつ下がる。
-      } else if (this.del_idBox[0].id === '') {
-        this.max_id = this.max_id + 1; // del_id が存在しない場合は、最大値＋１からインクリメントする。
-        this.result_id = this.max_id; // max_id　を　result_idに代入して、今回のid番号を確定させる。
-        this.del_idBox.shift();
-      } else if (this.del_idBox.length === 0) {
-        this.max_id = this.max_id + 1; // this.del_idBoxが存在しない場合。
+      // if( this.del_idBox.length !== 0 && this.del_idBox[0].id !== ''  ){
+      //   this.min_id = this.del_idBox[0].id;   // 最後に削除されたidを、min_idに代入。
+      //   this.result_id = this.min_id;         // min_id　を　result_idに代入して、今回のid番号を確定させる。
+      //   this.del_idBox.shift();               //配列の先頭の要素を削除　→　削除後は１番目以降の数字が一つづつ下がる。
+      //   //if( this.del_idBox.length === 0){
+      //   //  this.del_idBox[0].id = '';
+      //   //}
+      // }else if( this.del_idBox[0].id  === ''){
+      //   this.max_id = this.max_id + 1;        // del_id が存在しない場合は、最大値＋１からインクリメントする。
+      //   this.result_id = this.max_id;         // max_id　を　result_idに代入して、今回のid番号を確定させる。
+      //   //this.del_idBox.shift();
+      //   //this.del_idBox[0].id = 'hoge';
+      // }else if(this.del_idBox.length === 0){
+      //   this.max_id = this.max_id + 1;        // this.del_idBoxが存在しない場合。
+      //   this.result_id = this.max_id;
+      // }
+
+      if (this.del_idBox.length === 0) {
+        this.max_id = this.max_id + 1;
         this.result_id = this.max_id;
+      } else {
+        this.min_id = this.del_idBox[0].id;
+        this.result_id = this.min_id;
+        this.del_idBox.shift();
       }
+
       console.log('this.del_idBox.lengthは、『' + this.del_idBox.length + '』です。');
 
       // 通常の配列追加
@@ -355,11 +366,15 @@ new _vue2.default({
     list_delete: function list_delete(del) {
       console.log(del);
       var del_index = this.todo_list.length - 1 - del; // 配列の中で数字を反転させる計算。
-      //this.del_id = this.todo_list[del_index].id; //配列削除前に、削除予定idを、別建てに変数化しておく。
+
+      //配列削除前に、削除予定idを、別建てに配列変数化しておく。
       this.del_idBox.push({
-        id: this.todo_list[del_index].id //配列削除前に、削除予定idを、別建てに配列変数化しておく。
+        id: this.todo_list[del_index].id
       });
-      this.todo_list.splice(del_index, 1); //削除
+
+      //削除
+      this.todo_list.splice(del_index, 1);
+
       // 以下の文は、いささか不可解だが謎にうまく動いているので、一応良い。
       this.kensaku_todo_list.splice(del_index, 1); // この行は「del_index」ではなく「del」のような気がするが、「del_index」でうまくいく。なぞ。
     },
@@ -367,10 +382,18 @@ new _vue2.default({
     // 検索用配列を１つ削除した場合の、todo_list、kensaku_todo_listの同時削除(昇順ベースの削除)
     kensaku_list_delete: function kensaku_list_delete(del) {
       console.log(del);
-      // this.del_id = this.kensaku_todo_list[del].id; //配列削除前に、削除予定idを、別建てに変数化しておく。
+
+      //配列削除前に、削除予定idを、別建てにしなければならなが、格納予定先の配列に「hoge」が入っている場合は、削除して、配列のlengthを「0」にしておく必要がある。
+      if (this.del_idBox[0].id === '') {
+        this.del_idBox.shift();
+      }
+
+      //配列削除前に、削除予定idを、別建てに配列変数化しておく。
       this.del_idBox.push({
-        id: this.kensaku_todo_list[del].id //配列削除前に、削除予定idを、別建てに配列変数化しておく。
+        id: this.kensaku_todo_list[del].id
       });
+
+      //削除
       this.todo_list.splice(del, 1); //削除
       this.kensaku_todo_list.splice(del, 1);
       //this.del_id = this.del_id +1;
@@ -378,17 +401,18 @@ new _vue2.default({
 
     // check-box がonの時のbackground-colorの指定
     li_style: function li_style(itemId) {
+      console.log(itemId);
       if (!this.todo_list[itemId].checkbox) {
         this.todo_list[itemId].backgroundColor = '#C0C0C0';
         this.todo_list[itemId].completed_date = '(完了)' + Date();
-        this.kensaku_todo_list[itemId].checkbox = true;
-        this.kensaku_todo_list[itemId].backgroundColor = '#C0C0C0';
-        this.kensaku_todo_list[itemId].completed_date = '(完了)' + Date();
+        // this.kensaku_todo_list[itemId].checkbox = true;
+        // this.kensaku_todo_list[itemId].backgroundColor = '#C0C0C0';
+        // this.kensaku_todo_list[itemId].completed_date =  '(完了)' + Date();
       } else if (this.todo_list[itemId].checkbox) {
         this.todo_list[itemId].backgroundColor = '#FFFFFF';
-        this.kensaku_todo_list[itemId].checkbox = false;
-        this.kensaku_todo_list[itemId].backgroundColor = '#FFFFFF';
-        this.kensaku_todo_list[itemId].completed_date = '';
+        // this.kensaku_todo_list[itemId].checkbox = false;
+        // this.kensaku_todo_list[itemId].backgroundColor = '#FFFFFF';
+        // this.kensaku_todo_list[itemId].completed_date =  '';
       }
     },
     kensaku_li_style: function kensaku_li_style(itemId) {
